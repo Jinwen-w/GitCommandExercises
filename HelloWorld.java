abff864976050e6627d7e068be868e05cf5ace45
dlzqcxy <wangjinwen@hotmail.com>
2020-07-13 12:06:34 +0800

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,6 @@
 public class HelloWorld{
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hello World!冲突一");
         System.out.println("Github上第一次编辑！");
     }
 }
